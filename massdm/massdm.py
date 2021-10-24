6233673df8b5edaad94140cec671be9ea706f9e2
Craftzman7 <zezetta@icloud.com>
2021-10-24 11:50:19 -0700

    weeee

--- massdm/massdm.py
+++ massdm/massdm.py
@@ -14,9 +14,11 @@ class MassDmCog(commands.Cog):
     async def massdmrole(self, ctx: commands.Context, role: discord.Role, *, message: str) -> None:
         """Send messages to members with a specified role"""
         
+        message = await ctx.send("Starting DMs")
 
         for member in [e for e in role.members]:
             try:
+                await message.edit(f content="Sending message to {member}", embed=None)
                 await member.send(message.format(member=member, role=role, server=ctx.guild, sender=ctx.author))
             except discord.Forbidden:
                 ctx.send(f"Error sending a message to {member} due to insufficient permissions")
@@ -34,7 +36,7 @@ class MassDmCog(commands.Cog):
         message = await ctx.send("Starting DMs")
         for member in ctx.guild.members:
             try:
-                await message.edit(f"Sending message to {member}")
+                await message.edit(f content="Sending message to {member}", embed=None)
                 await member.send(message.format(member=member, server=ctx.guild, sender=ctx.author))
             except discord.Forbidden:
                 ctx.send(f"Error sending a message to {member} due to insufficient permissions")
