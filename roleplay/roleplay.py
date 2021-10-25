c6ed5516b4e3a2785e97c094c92afb1e78d48565
Crafterzman <zezetta@icloud.com>
2021-10-25 16:56:15 +0000

    lets try this

---
+++ roleplay/roleplay.py
@@ -0,0 +1,24 @@
+import discord
+import nekos
+
+from redbot.core import commands
+
+class Roleplay(commands.Cog):
+    """Roleplay cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command
+    @commands.guild_only()
+    async def slap(self, ctx, *, user: discord.Member):
+        """Slap another user"""
+        if user == ctx.author:
+            return await ctx.send("You can't slap your self!")
+        try:
+            embed = discord.Embed(title="Ouch!", color=0x00ff00)
+            url = nekos.img("slap")
+            embed.set_image(url=url)
+            await ctx.send(embed=embed)
+
+        
